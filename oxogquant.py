3c755529fac9471b68dea17955b4be0e715ef6db
s-andrews <simon@proteo.me.uk>
2025-10-07 09:25:54 +0100

    Basic quantitation implemented

--- oxogquant.py
+++ oxogquant.py
@@ -6,25 +6,89 @@
 ## observed Gs in the data along with the frequency of G to T transversions
 
 import argparse
+import pysam
+from pathlib import Path
+
 
 options = None
 
 def main():
     get_options()
 
+
+
     # Structure of the quantitations will be
     # quantitation = {"chr:pos:strand": {bamfile:[totalcount,gt_count]}}
     quantitations = {}
 
     for bamfile in options.bamfiles:
-        process_bamfile(bamfile,quantitations)
+        process_bamfile(Path(bamfile),quantitations)
+
+    breakpoint()
 
     write_output(quantitations)
 
 
-def process_bamfile(file,quantitaitons):
-    pass
+def process_bamfile(file,quantitations):
+    bamin = pysam.AlignmentFile(file, "rb")
 
+    for i,read in enumerate(bamin.fetch(until_eof=True)):
+
+        if i>0 and i % 100000 == 0:
+            print(f"Read {i} reads", flush=True)
+            break ##### JUST TESTING ########
+        
+
+        # We need to figure out the direction we're looking in
+        # ie looking for G or C
+        # 
+        reverse = options.direction=="opposing"
+
+        chr = read.reference_name
+        strand = "+"
+
+        if read.is_reverse:
+            reverse = not reverse
+            strand = "-"
+
+        seeking = "G"
+        checking = "T"
+
+        if reverse:
+            seeking = "C"
+            checking = "A"
+
+        query_seq = read.query_sequence
+
+        for query_zero_pos, ref_zero_pos, ref_base in read.get_aligned_pairs(with_seq=True):
+            if ref_base != seeking:
+                continue
+
+            # There may not be an alignment for this position
+            if query_zero_pos is None:
+                continue
+
+            # We have a position we want to record
+            modified = query_seq[query_zero_pos] == checking
+
+            # Our genome position will be the reported position plus one (it's zero indexed)
+
+            position = ref_zero_pos+1
+                
+            locus = chr+":"+str(position)+":"+strand
+
+            if not locus in quantitations:
+                quantitations[locus] = {}
+
+            if not file.name in quantitations[locus]:
+                quantitations[locus][file.name] = [0,0]
+
+            quantitations[locus][file.name][0] += 1 # We saw this base
+            
+            if modified:
+                quantitations[locus][file.name][1] += 1 # The base was modified
+
+            
 
 def get_options():
 
@@ -37,3 +101,8 @@ def get_options():
 
     global options
     options = parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    main()
